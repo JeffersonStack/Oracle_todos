373d8908458ec021b1e8ad3eb8f12912592432b0
Jefferson Teles <jefferson_stack@outlook.com>
2020-01-17 19:16:22 -0300

    Projeto oracle todos finalizado

--- js/main.js
+++ js/main.js
@@ -7,11 +7,14 @@ let boxList = document.querySelector(".box-list");
 
 // List Todos
 
-let todos = ["Estudar Javascript", "Treinar Musculação", "Terminar Projeto"];
+let todos = JSON.parse(localStorage.getItem("list-items")) || [];
 
 // Get Todos
 
 function exibirTodos() {
+  // clear Todo
+  listElement.innerHTML = "";
+
   todos.forEach(todo => {
     // Create Elements
     let addElement = document.createElement("li");
@@ -24,6 +27,10 @@ function exibirTodos() {
     addElement.setAttribute("class", "list-group-item");
     linkElement.setAttribute("class", "right-element");
     iconElement.setAttribute("class", "fas fa-trash-alt");
+
+    // Select todos position
+    let pos = todos.indexOf(todo);
+    linkElement.setAttribute("onclick", "removeTodo(" + pos + ")");
 
     // Todo Text
     let todoText = document.createTextNode(todo);
@@ -40,6 +47,28 @@ exibirTodos();
 
 // Add Todos
 
+function addTodos() {
+  let textTodos = inputElement.value;
+  todos.push(textTodos);
+  inputElement.value = "";
+
+  exibirTodos();
+  saveTodos();
+}
+
+buttonElement.addEventListener("click", () => {
+  addTodos();
+});
+
 // Remove Todos
 
+function removeTodo(pos) {
+  todos.splice(pos, 1);
+  exibirTodos();
+  saveTodos();
+}
+
 // Save Todos
+function saveTodos() {
+  localStorage.setItem("list-items", JSON.stringify(todos));
+}
