86f14ae718c0f769266cc975ec217a77119f3f98
Jefferson Teles <jefferson_stack@outlook.com>
2020-01-17 17:39:03 -0300

    Projeto Oracle Todos

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// DOM Elements
+
+let inputElement = document.querySelector("#inputText");
+let buttonElement = document.querySelector("button");
+let listElement = document.querySelector("#list-todo");
