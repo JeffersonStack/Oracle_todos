29e4b8dfee0b7ade8d4a7a4adcf58369ab9e0046
Jefferson Teles <jefferson_stack@outlook.com>
2020-01-17 18:52:50 -0300

    Show list of tasks in the body of the page

--- js/main.js
+++ js/main.js
@@ -3,3 +3,43 @@
 let inputElement = document.querySelector("#inputText");
 let buttonElement = document.querySelector("button");
 let listElement = document.querySelector("#list-todo");
+let boxList = document.querySelector(".box-list");
+
+// List Todos
+
+let todos = ["Estudar Javascript", "Treinar Musculação", "Terminar Projeto"];
+
+// Get Todos
+
+function exibirTodos() {
+  todos.forEach(todo => {
+    // Create Elements
+    let addElement = document.createElement("li");
+    let linkElement = document.createElement("a");
+    let iconElement = document.createElement("i");
+    linkElement.setAttribute("href", "#");
+    let linkText = iconElement;
+
+    // Atributtes class list
+    addElement.setAttribute("class", "list-group-item");
+    linkElement.setAttribute("class", "right-element");
+    iconElement.setAttribute("class", "fas fa-trash-alt");
+
+    // Todo Text
+    let todoText = document.createTextNode(todo);
+
+    // Body Todos
+    linkElement.appendChild(linkText);
+    addElement.appendChild(todoText);
+    addElement.appendChild(linkElement);
+    listElement.appendChild(addElement);
+  });
+}
+
+exibirTodos();
+
+// Add Todos
+
+// Remove Todos
+
+// Save Todos
